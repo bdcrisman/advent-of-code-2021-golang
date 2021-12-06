1d40a1dd4d13bfd5cdaa20b3831ee682b8e91300
Ben Crisman <benjamin.crisman@gmail.com>
2021-12-06 00:04:16 -0700

    day 1 part 1 example correct

--- day1/day1.go
+++ day1/day1.go
@@ -6,18 +6,33 @@ import (
 	"github.com/bdcrisman/advent-of-code/2021/utils"
 )
 
-func Day1(inputPath string) []int {
+// Gets the number of increases occurring in the data.
+func Day1Part1(inputPath string) int {
 	if inputPath == "" {
 		println("DAY1: no input path given")
-		return nil
+		return 0
 	}
 
 	path := filepath.Join("day1", inputPath)
 	data, err := utils.ReadAsIntArray(path)
 	if err != nil {
 		println(err.Error())
-		return nil
+		return 0
 	}
 
-	return data
+	return countNumIncreases(data)
+}
+
+func countNumIncreases(data []int) int {
+	var count int
+	last := data[0]
+
+	for _, i := range data {
+		if i > last {
+			count++
+		}
+		last = i
+	}
+
+	return count
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bdcrisman/advent-of-code/2021/day1"
 )
 
 func main() {
-	d1 := day1.Day1("example-input")
-	for _, i := range d1 {
-		println(i)
-	}
+	d1p1 := day1.Day1Part1("example-input")
+	fmt.Printf("Day 1 Part 1 :: Number increases: %d\n", d1p1)
 }
