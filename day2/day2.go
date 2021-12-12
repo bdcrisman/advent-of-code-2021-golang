4e5c196ad09d95dd23e999d023cd2840d1fab64b
Ben Crisman <benjamin.crisman@gmail.com>
2021-12-12 16:48:49 -0700

    part 1 complete

--- day2/day2.go
+++ day2/day2.go
@@ -1,5 +1,52 @@
 package day2
 
-func Day2(inputPath string) int {
-	return 1
+import (
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/bdcrisman/advent-of-code/2021/utils"
+)
+
+func Day2(inputPath string, isPart1 bool) int {
+	path := filepath.Join("day2", inputPath)
+	data, err := utils.ReadAsStringArray(path)
+	if err != nil {
+		println(err.Error())
+		return 0
+	}
+
+	return calculateCommands(data)
+}
+
+func calculateCommands(lines []string) int {
+	var hpos int
+	var depth int
+
+	for _, line := range lines {
+		cmd := parseCommand(line)
+
+		if strings.Contains(line, "forward") {
+			hpos += cmd
+		} else if strings.Contains(line, "up") {
+			depth -= cmd
+		} else if strings.Contains(line, "down") {
+			depth += cmd
+		}
+	}
+
+	return hpos * depth
+}
+
+func parseCommand(line string) int {
+	s := strings.Split(line, " ")
+	if len(s) < 2 {
+		return 0
+	}
+
+	v, err := strconv.Atoi(s[1])
+	if err != nil {
+		return 0
+	}
+	return v
 }
--- utils/read-write.go
+++ utils/read-write.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"bufio"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -25,6 +26,32 @@ func ReadAsIntArray(path string) ([]int, error) {
 	}
 
 	return result, scanner.Err()
+}
+
+// Reads file in as a string
+func ReadAsString(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+// Reads file in as string array.
+// https://stackoverflow.com/questions/5884154/read-text-file-into-string-array-and-write
+func ReadAsStringArray(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
 // Reads in file and returns the scanner.
